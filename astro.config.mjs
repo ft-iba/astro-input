a54d46ed77604900b5f9c10d197ce3f235b282f7
daisuke iba <d.iba@futurek.com>
2025-04-18 10:51:54 +0900

    https://docs.astro.build/ja/guides/deploy/github/

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import relativeLinks from 'astro-relative-links';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ft-iba.github.io',
+  base: 'astro-input',
   integrations: [relativeLinks()]
 });

d6ee25cb832b6ec9aa5f2006f302d6124182566d
daisuke iba <d.iba@futurek.com>
2025-04-18 18:25:12 +0900

    publicディレクトリの画像にコミットハッシュでバージョン付与

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { execSync } from 'child_process';
 import { site } from './src/constants';
+
+const commitHash = execSync('git rev-parse --short HEAD').toString().trim();
 
 // https://astro.build/config
 export default defineConfig({
   site: site.domain,
   base: site.base,
   trailingSlash: 'always',
+  vite: {
+    define: {
+      'import.meta.env.COMMIT_HASH': JSON.stringify(commitHash),
+    },
+  },
 });
